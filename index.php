3a56ec52a27dee5974950103b8b50b02f77f649c
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-09-06 15:10:40 +0200

    start page done , register page in progress

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php include('pages/header.php')?>
+<body class="flex flex-col justify-center items-center w-screen min-h-screen gap-20">
+<img src="assets/img/logoBlaBlaFirstPage.png" alt="Logo de BlaBla Campus">
+<div id="containerStarting" class="w-4/5">
+    <a href="pages/register.php" id="starting" class="flex w-full justify-center items-center gap-2 BGColorRedOnline">
+        <img src="assets/img/carStarting.png" alt="Une voiture">
+        <p class="text-white workSans ls5">COMMENCEZ</p>
+    </a>
+    <a href="pages/login.php" class="flex justify-center items-center w-full"><p class="workSans ls5 colorRedOnline">SE CONNECTER</p></a>
+</div>
 <?php include('pages/footer.php')?>
