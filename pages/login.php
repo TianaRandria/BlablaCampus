2a0230b717a843c6bf327ac548fcc0822e748971
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-09-08 13:40:48 +0200

    end login.php front end , need review

--- pages/login.php
+++ pages/login.php
@@ -2,8 +2,8 @@
 <form action="" method="post" class="flex flex-col w-full p-4 gap-9">
     <label for="login" class="bungee text-lg">Entrez vos informations</label>
     <div class="flex flex-col gap-2.5">
-        <input type="text" name="login" placeholder="Nom d'utilisateur" class="BGColorLightGrey roundBorder epilogue p-3">
-        <input type="password" name="password" id="password" placeholder="******************" class="BGColorLightGrey roundBorder p-3">
+        <input type="text" name="login" placeholder="Nom d'utilisateur" class="BGColorLightGrey roundBorder epilogue p-3" required>
+        <input type="password" name="password" id="password" placeholder="******************" class="BGColorLightGrey roundBorder p-3" required>
     </div>
     <div class="flex flex-col items-center justify-center w-full gap-9">
         <input type="submit" value="SE CONNECTER" class="BGColorRedOnline roundBorder ls5 workSans text-white text-sm w-3/4 py-2">
