f20db75db47e573e30b80f764f7b2b44314d9f30
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-09-09 15:39:35 +0200

    searchItinerary end , might need review

--- pages/searchItinerary.php
+++ pages/searchItinerary.php
@@ -2,15 +2,15 @@
 <div class="w-4/5 p-2 flex">
     <h2 class="bungee">Rechercher un trajet</h2>
 </div>
-<form action="" method="post" id="searchItinerary" class="w-4/5">
+<form action="" method="post" id="searchItinerary" class="w-4/5 flex flex-col gap-3 justify-center items-center">
     <div class="BGColorLightGrey w-full flex roundBorder h-14">
-        <div class="p-3">
+        <div class="p-3 w-1/5 flex justify-center items-center">
             <img src="../assets/img/pinPoint.png" alt="Cible d'un lieu">
         </div>
         <input type="text" name="startingPointSearch" id="startingPointSearch" placeholder="Départ" class="BGColorLightGrey w-4/5 epilogue font-medium text-base">
     </div>
     <div class="BGColorLightGrey w-full flex roundBorder h-14">
-        <div class="p-3">
+        <div class="p-3 w-1/5 flex justify-center items-center">
             <img src="../assets/img/pinPoint.png" alt="Cible d'un lieu">
         </div>
         <select name="arrivalPointSearch" id="arrivalPointSearch" class="w-4/5 BGColorLightGrey">
@@ -18,7 +18,16 @@
             <option value="2 Route de Montaigu, 39000 Lons-le-Saunier">2 Rte de Montaigu, 39000 Lons-le-Saunier</option>
         </select>
     </div>
-    <label for="dateSearch" class="BGColorLightGrey w-full">Test</label>
-    <input type="date" name="dateSearch" id="dateSearch" class="BGColorLightGrey w-full h-14">
+    <div class="w-full h-14 BGColorLightGrey" id="blocDateSearch">
+        <div id="firstRow" class="w-full h-full flex justify-start items-center">
+            <div class="w-1/5 flex justify-center items-center">
+                <img src="../assets/img/calendar.png" alt="logo de calendrier">
+            </div>
+            <p class="w-4/5">Aujourd'hui</p>
+        </div>
+        
+        <input type="date" name="dateSearch" id="dateSearch" class="BGColorLightGrey w-full h-14 dsn">
+    </div>
+    <input type="submit" value="RECHERCHER" class="ls5 workSans p-3 w-4/5 BGColorRedOnline roundBorder text-white">
 </form>
 <?php include('footer.php')?>
