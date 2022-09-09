9c35b192a79b8d642f2ad0ee6da6da5a4a572029
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-09-09 16:30:08 +0200

    Update resultSearch.php

--- pages/resultSearch.php
+++ pages/resultSearch.php
@@ -1,3 +1,13 @@
 <?php include('header.php')?>
-
+<div>
+    <div>
+        <p class="jourDate"></p>
+        <p class="moisDate"></p>
+    </div>
+    <div>
+        <p class="startingPoint"></p>
+        <p class="endingPoint"></p>
+    </div>
+    <img src="" alt="" class="fleche">
+</div>
 <?php include('footer.php')?>
