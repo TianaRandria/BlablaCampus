d8fd1c66bf7d63dfa075fcaf32a06d70f7d67397
Antoine Piron <53710517+Whitenor@users.noreply.github.com>
2022-09-13 11:57:49 +0200

    update card Search Result

--- pages/resultSearch.php
+++ pages/resultSearch.php
@@ -24,10 +24,47 @@
 
     <p class="w-5/6"><span class="colorRedOnline">5</span> trajets disponibles</p>
 
+    <!-- texte fixe ou aucune modif n'est nécessaireok -->
+
     <div class="w-5/6 flex justify-between items-center gap-2">
         <img src="../assets/img/clock.png" alt="Logo d'horloge">
         <p class="epilogue font-medium text-xs">Les trajets sont triés chronologiquement par heure de départ.</p>
     </div>
+
+    <!--     Template pour les cards que tu devras générer via tes fonctions de recherches -->
+
+
+    <!-- les endroits à changer avec tes fonctions sont le nombre de place disponibles ( le chiffre uniquement ) , les lieux de destinations/départ , l'image du conducteur , son nom et la petite phrase. Pour l'image de l'utilisateur si il te faut une autre balise qu'une img hésite pas à me prévenir , que je puisse adapter ça proprement -->
+    <div class="card w-5/6 BGColorLightGrey roundBorder flex flex-col p-3 gap-3.5">
+        <p class="text-xs workSans text-end w-full">places disponibles : <span class="colorRedOnline font-bold">2</span></p>
+        <div class="firstRow w-full flex h-12 gap-2">
+            <div class="flex flex-col justify-between h-full">
+                <p class="colorRedOnline font-bold text-sm">6H30</p>
+                <p class="colorRedOnline font-bold text-sm">7H30</p>
+            </div>
+            <div class="flex flex-col relative justify-between h-full">
+                <span class="circleSearchResult"></span>
+                <span class="circleSearchResult"></span>
+                <span class="blackBar absolute"></span>
+            </div>
+            <div class="h-full flex flex-col justify-between items-start">
+                <p class="colorFakeBlack font-bold epilogue text-sm">Dole</p>
+                <p class="colorFakeBlack font-bold epilogue text-sm">Lons le Saunier</p>
+            </div>
+        </div>
+        <div class="secondRow flex gap-3 justify-start items-center">
+            <div class="first-col">
+                <img src="../assets/img/humanLogo.png" alt="img du conducteur" class="img-cardSearch">
+            </div>
+            <div class="second-col">
+                <p class="epilogue text-sm font-bold">Pauline</p>
+                <p class="epilogue font-light text-xs italic">Avec moi ça passe ou ça casse</p>
+            </div>
+        </div>
+    </div>
+
+
+
 </main>
 
 <?php include('footer.php')?>
