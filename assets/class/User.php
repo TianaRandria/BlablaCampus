1e0dffaa13eda16e7636ebae5b941af10d27741e
VincentBenoit39 <vincent-benoit@outlook.fr>
2022-09-14 16:27:45 +0200

    user register

--- assets/class/User.php
+++ assets/class/User.php
@@ -27,6 +27,8 @@ class User extends Database
     $password = password_hash($_POST['pswdRegister'],  PASSWORD_DEFAULT);
     $email = $_POST['emailRegister'];
     $bio = $_POST['bioRegister'];
+    $file = $_FILES['profilePictureRegister']['tmp_name'];
+    $img = addslashes(file_get_contents($file));
     $connection = $this->connect()->prepare("SELECT * FROM compte WHERE nickname_user = :nickname_user");
     $connection->bindParam(':nickname_user', $nickname, PDO::PARAM_STR);
     $connection->execute();
@@ -34,12 +36,13 @@ class User extends Database
     if ($exist == true) {
       echo "desolé cet identifiant existe déja";
     } else {
-      $register = $this->connect()->prepare("INSERT INTO compte (name_user, nickname_user, password_user, email_user, bio_user) VALUES (:name_user, :nickname_user, :password_user, :email_user, :bio_user)");
+      $register = $this->connect()->prepare("INSERT INTO compte (name_user, nickname_user, password_user, email_user, bio_user, img_user) VALUES (:name_user, :nickname_user, :password_user, :email_user, :bio_user, :img_user)");
       $register->bindParam(':name_user', $name, PDO::PARAM_STR);
       $register->bindParam(':nickname_user', $nickname, PDO::PARAM_STR);
       $register->bindParam(':password_user', $password, PDO::PARAM_STR);
       $register->bindParam(':email_user', $email, PDO::PARAM_STR);
       $register->bindParam(':bio_user', $bio, PDO::PARAM_STR);
+      $register->bindParam(':img_user', $img, PDO::PARAM_STR);
       $register->execute();
       $_SESSION['name_user'] = $nickname;
     }
