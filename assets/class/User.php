fb7846b517206004bb0cf37da982578e27210c6d
VincentBenoit39 <vincent-benoit@outlook.fr>
2022-09-15 16:28:45 +0200

    user fix register

--- assets/class/User.php
+++ assets/class/User.php
@@ -27,13 +27,15 @@ class User extends Database
     $password = password_hash($_POST['pswdRegister'],  PASSWORD_DEFAULT);
     $email = $_POST['emailRegister'];
     $bio = $_POST['bioRegister'];
-    $img = file_get_contents($_POST['profilePictureRegister']);
-    $connection = $this->connect()->prepare("SELECT * FROM compte WHERE nickname_user = :nickname_user");
+    $img = file_get_contents($_FILES['profilePictureRegister']);
+    $connection = $this->connect()->prepare("SELECT * FROM compte WHERE nickname_user, email_user = :nickname_user, :email_user");
     $connection->bindParam(':nickname_user', $nickname, PDO::PARAM_STR);
+    $connection->bindParam(':email_user', $email, PDO::PARAM_STR);
     $connection->execute();
     $exist = $connection->fetch();
     if ($exist != false) {
-      echo "desolé cet identifiant existe déja";
+      // echo "desolé cet identifiant existe déja";
+      header('Location: ./register.php');
     } else {
       $register = $this->connect()->prepare("INSERT INTO compte (name_user, nickname_user, password_user, email_user, bio_user, img_user) VALUES (:name_user, :nickname_user, :password_user, :email_user, :bio_user, :img_user)");
       $register->bindParam(':name_user', $name, PDO::PARAM_STR);
@@ -51,5 +53,24 @@ class User extends Database
   {
     session_destroy();
     header('Location: ./index.php');
+  }
+  public function pswdReset()
+  {
+    if (isset($_POST['email'])) {
+      $password = uniqid();
+      $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+      $subject = 'Mot de passe oublié';
+      $message = "Bonjour, voici votre nouveau mot de passe : $password";
+      $headers = 'Content-Type: text/plain; charset="UTF-8"';
+
+      if (mail($_POST['email'], $subject, $message, $headers)) {
+        $stmt = $this->connect()->prepare("UPDATE users SET password = ? WHERE email = ?");
+        $stmt->execute([$hashedPassword, $_POST['email']]);
+        echo "E-mail envoyé";
+      } else {
+        echo "Une erreur est survenue";
+      }
+    }
   }
 }
