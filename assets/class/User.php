2b31a89550c94a76236804b4cdb35dff6f1f4131
VincentBenoit39 <vincent-benoit@outlook.fr>
2022-09-15 14:24:09 +0200

    Register aprouved

--- assets/class/User.php
+++ assets/class/User.php
@@ -27,30 +27,18 @@ class User extends Database
     $password = password_hash($_POST['pswdRegister'],  PASSWORD_DEFAULT);
     $email = $_POST['emailRegister'];
     $bio = $_POST['bioRegister'];
-    $img = $_FILES['profilePictureRegister']['name'];
-
-    $connection = $this->connect()->prepare("SELECT * FROM compte WHERE nickname_user = :nickname_user");
-    $connection->bindParam(':nickname_user', $nickname, PDO::PARAM_STR);
-    $connection->execute();
-    $exist = $connection->fetch();
-
-    if (empty($_FILES)) {
-      echo "Votre fichier n'est pas une image, seul les extensions Jpg, png et gif sont acceptées";
-    } else {
-      if ($exist == true) {
-        echo "desolé cet identifiant existe déja";
-      } else {
-        $register = $this->connect()->prepare("INSERT INTO compte (name_user, nickname_user, password_user, email_user, bio_user, img_user) VALUES (:name_user, :nickname_user, :password_user, :email_user, :bio_user, :img_user)");
-        $register->bindParam(':name_user', $name, PDO::PARAM_STR);
-        $register->bindParam(':nickname_user', $nickname, PDO::PARAM_STR);
-        $register->bindParam(':password_user', $password, PDO::PARAM_STR);
-        $register->bindParam(':email_user', $email, PDO::PARAM_STR);
-        $register->bindParam(':bio_user', $bio, PDO::PARAM_STR);
-        $register->bindParam(':img_user', $img, PDO::PARAM_STR);
-        $register->execute();
-        $_SESSION['name_user'] = $nickname;
-      }
-    }
+    $register = $this->connect()->prepare("INSERT INTO compte (name_user, nickname_user, password_user, email_user, bio_user) VALUES (:name_user, :nickname_user, :password_user, :email_user, :bio_user)");
+    $register->bindParam(':name_user', $name, PDO::PARAM_STR);
+    $register->bindParam(':nickname_user', $nickname, PDO::PARAM_STR);
+    $register->bindParam(':password_user', $password, PDO::PARAM_STR);
+    $register->bindParam(':email_user', $email, PDO::PARAM_STR);
+    $register->bindParam(':bio_user', $bio, PDO::PARAM_STR);
+    $register->execute();
+    var_dump($name);
+    var_dump($nickname);
+    var_dump($password);
+    var_dump($email);
+    var_dump($bio);
   }
 
   public function logout()
