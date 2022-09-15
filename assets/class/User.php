8f25c49485c354c3a18bb9b3ea57eaf703c91f2f
VincentBenoit39 <vincent-benoit@outlook.fr>
2022-09-15 11:30:17 +0200

    add photo

--- assets/class/User.php
+++ assets/class/User.php
@@ -27,24 +27,29 @@ class User extends Database
     $password = password_hash($_POST['pswdRegister'],  PASSWORD_DEFAULT);
     $email = $_POST['emailRegister'];
     $bio = $_POST['bioRegister'];
-    $file = $_FILES['profilePictureRegister']['tmp_name'];
-    $img = addslashes(file_get_contents($file));
+    $img = $_FILES['profilePictureRegister']['name'];
+
     $connection = $this->connect()->prepare("SELECT * FROM compte WHERE nickname_user = :nickname_user");
     $connection->bindParam(':nickname_user', $nickname, PDO::PARAM_STR);
     $connection->execute();
     $exist = $connection->fetch();
-    if ($exist == true) {
-      echo "desolé cet identifiant existe déja";
+
+    if (empty($_FILES)) {
+      echo "Votre fichier n'est pas une image, seul les extensions Jpg, png et gif sont acceptées";
     } else {
-      $register = $this->connect()->prepare("INSERT INTO compte (name_user, nickname_user, password_user, email_user, bio_user, img_user) VALUES (:name_user, :nickname_user, :password_user, :email_user, :bio_user, :img_user)");
-      $register->bindParam(':name_user', $name, PDO::PARAM_STR);
-      $register->bindParam(':nickname_user', $nickname, PDO::PARAM_STR);
-      $register->bindParam(':password_user', $password, PDO::PARAM_STR);
-      $register->bindParam(':email_user', $email, PDO::PARAM_STR);
-      $register->bindParam(':bio_user', $bio, PDO::PARAM_STR);
-      $register->bindParam(':img_user', $img, PDO::PARAM_STR);
-      $register->execute();
-      $_SESSION['name_user'] = $nickname;
+      if ($exist == true) {
+        echo "desolé cet identifiant existe déja";
+      } else {
+        $register = $this->connect()->prepare("INSERT INTO compte (name_user, nickname_user, password_user, email_user, bio_user, img_user) VALUES (:name_user, :nickname_user, :password_user, :email_user, :bio_user, :img_user)");
+        $register->bindParam(':name_user', $name, PDO::PARAM_STR);
+        $register->bindParam(':nickname_user', $nickname, PDO::PARAM_STR);
+        $register->bindParam(':password_user', $password, PDO::PARAM_STR);
+        $register->bindParam(':email_user', $email, PDO::PARAM_STR);
+        $register->bindParam(':bio_user', $bio, PDO::PARAM_STR);
+        $register->bindParam(':img_user', $img, PDO::PARAM_STR);
+        $register->execute();
+        $_SESSION['name_user'] = $nickname;
+      }
     }
   }
 
